27f992dce68cb23ffcba57d7ec1c99e251d21f93
Supremehamster <888ham@gmail.com>
2017-07-11 10:45:22 +0100

    run a url

---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            System.Diagnostics.Process.Start("http://m-b-a.biz#last")
+        Catch
+            'Code to handle the error.
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Application.Exit()
+        End
+    End Sub
+End Class
